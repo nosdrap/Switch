7d0917cc3807ab75016cc66bea1c51fbf44e22ed
nosdrap <61735713+nosdrap@users.noreply.github.com>
2020-07-07 15:42:57 +0200

    removed comment

--- switch.py
+++ switch.py
@@ -3,5 +3,3 @@ def switch(variable,array):
     for x in array:
         if variable == x[0] or x[0] == "default":
             return x[1]
-# variable you want to switch
-# array = [[value,function_name]["default",function_name]
